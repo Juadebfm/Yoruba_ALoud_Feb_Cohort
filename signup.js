dcf8ff727f04f484eaa5242fa7ee2f93f25a702c
Juadeb <juadebgabriel@gmail.com>
2023-06-08 22:44:07 +0100

    fix

--- signup.js
+++ signup.js
@@ -1,80 +1,76 @@
-//Step 1- get all the neccessary elements
 const signUpBtn = document.getElementById("SignUp");
-const getName = document.getElementById("name");
-const getEmail = document.getElementById("email");
-const getPassword = document.getElementById("password");
-const getConfirmPassword = document.getElementById("confirmPassword");
 
-//add a text to the button
 signUpBtn.innerText = "Sign Up";
 
-//add event listener to the button
 signUpBtn.addEventListener("click", (event) => {
   event.preventDefault();
 
-  //checks if any of the fields are empty
+  const getName = document.getElementById("name").value;
+  const getEmail = document.getElementById("email").value;
+  const getPassword = document.getElementById("password").value;
+  const getConfirmPassword = document.getElementById("confirmPassword").value;
+
+  signUpBtn.innerText = "Loading";
+
   if (
-    getName.value === "" ||
-    getEmail.value === "" ||
-    getPassword.value === "" ||
-    getConfirmPassword.value === ""
+    getName === "" ||
+    getEmail === "" ||
+    getPassword === "" ||
+    getConfirmPassword === ""
   ) {
+    // Display error message if any field is empty
     Swal.fire({
       icon: "info",
       text: "All fields are required!",
       confirmButtonColor: "#2D85DE",
     });
-    signUpBtn.style.fontStyle = "italic";
-    signUpBtn.disabled = "true";
-  }
-
-  //check if the confirm password is equal to the password
-  if (getConfirmPassword !== getPassword) {
+    signUpBtn.innerText = "Sign Up";
+  } else if (getConfirmPassword !== getPassword) {
+    // Display error message if passwords do not match
     Swal.fire({
       icon: "info",
-      text: "Password do not match",
+      text: "Password does not match",
       confirmButtonColor: "#2D85DE",
     });
-    signUpBtn.style.fontStyle = "italic";
-    signUpBtn.disabled = "true";
+    signUpBtn.innerText = "Sign Up";
   } else {
-    // now append the values of the inputs to the form data
-    const signUpData = new FormData();
-    signUpData.append("name", getName);
-    signUpData.append("email", getEmail);
-    signUpData.append("password", getPassword);
-    signUpData.append("password_confirmation", getConfirmPassword);
+    // Create a FormData object and append form data
+    const signData = new FormData();
+    signData.append("name", getName);
+    signData.append("email", getEmail);
+    signData.append("password", getPassword);
+    signData.append("password_confirmation", getConfirmPassword);
 
-    // provide the body information for the API
-    const signUpRequirement = {
+    const signReq = {
       method: "POST",
-      body: signUpData,
+      body: signData,
     };
+
+    const url =
+      "https://pluralcodesandbox.com/yorubalearning/api/register_admin";
+
+    fetch(url, signReq)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.status === "success") {
+          Swal.fire({
+            icon: "success",
+            text: `${result.message}`,
+            confirmButtonColor: "#2D85DE",
+          });
+
+          setTimeout(() => {
+            location.href = "index.html";
+          }, 3000);
+        } else {
+          Swal.fire({
+            icon: "info",
+            text: "Registration Unsuccessful!",
+            confirmButtonColor: "#2D85DE",
+          });
+          signUpBtn.innerText = "Sign Up";
+        }
+      })
+      .catch((error) => console.log("error", error));
   }
-
-  //get the api url
-  const url = "https://pluralcodesandbox.com/yorubalearning/api/register_admin";
-
-  fetch(url, signUpRequirement)
-    .then((response) => response.json())
-    .then((result) => {
-      if (result.status === "success") {
-        Swal.fire({
-          icon: "success",
-          text: `${result.message}`,
-          confirmButtonColor: "#2D85DE",
-        });
-        setTimeout(() => {
-          location.href = "./index.html";
-        }, 3000);
-      } else {
-        Swal.fire({
-          Icon: "info",
-          text: "Registration Unsuccessful!",
-          confirmButtonColor: "#2D85DE",
-        });
-        signUpBtn.innerHTML = "Try Again";
-      }
-    })
-    .catch((error) => console.log("error", error));
 });
